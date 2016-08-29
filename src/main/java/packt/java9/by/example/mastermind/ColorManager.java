151f00fb603dccaba3f46d060aa02bb046f2ca85
Peter Verhas <peter@verhas.com>
2016-08-29 22:50:24 +0200

    comments that were dead code are deleted commenting out code is bad practice. use version control instead

--- src/main/java/packt/java9/by/example/mastermind/ColorManager.java
+++ src/main/java/packt/java9/by/example/mastermind/ColorManager.java
@@ -7,8 +7,6 @@ public class ColorManager {
     final protected int nrColors;
     final protected Map<Color, Color> successor = new HashMap<>();
     final private Color first;
-    //final List<Color> colors = new ArrayList<>();
-
 
     public ColorManager(int nrColors) {
         this.nrColors = nrColors;
@@ -22,26 +20,11 @@ public class ColorManager {
         successor.put(previousColor, Color.none);
     }
 
-    /*
-        public ColorManager(int nrColors) {
-            this.nrColors = nrColors;
-            for (int i = 0; i < nrColors; i++) {
-                colors.add(new Color());
-            }
-            first = colors.get(0);
-            colors.add(Color.none);
-        }
-    */
     public Color firstColor() {
         return first;
     }
 
     Color nextColor(Color color) {
-        /*
-        if (color == Color.none)
-            return null;
-        else
-            return colors.get(colors.indexOf(color) + 1);*/
         return successor.get(color);
     }
 }
